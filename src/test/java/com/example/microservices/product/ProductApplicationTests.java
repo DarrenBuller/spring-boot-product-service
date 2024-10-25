fadcf54e1f717fbd227d1794c3e554d03ac20330
Darren Buller <stash-taco-remake@duck.com>
2024-10-25 16:18:24 +0100

    Implement product service
    Code uses mongo db in a docker container

--- src/test/java/com/example/microservices/product/ProductApplicationTests.java
+++ src/test/java/com/example/microservices/product/ProductApplicationTests.java
@@ -1,15 +1,73 @@
 package com.example.microservices.product;
 
+import java.math.BigDecimal;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MongoDBContainer;
+
+import com.example.microservices.product.dto.ProductRequest;
+
+import io.restassured.RestAssured;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
 
+	@ServiceConnection
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.7");
+	@LocalServerPort
+	private Integer port;
+
+	@BeforeEach
+	void setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+
+	static {
+		mongoDBContainer.start();
+	}
+
+	@Test
+	void shouldCreateProduct() throws Exception {
+		ProductRequest productRequest = getProductRequest();
+
+		RestAssured.given()
+				.contentType("application/json")
+				.body(productRequest)
+				.when()
+				.post("/api/product")
+				.then()
+				.log().all()
+				.statusCode(201)
+				.body("id", Matchers.notNullValue())
+				.body("name", Matchers.equalTo(productRequest.name()))
+				.body("description", Matchers.equalTo(productRequest.description()))
+				.body("price", Matchers.is(productRequest.price().intValueExact()));
+	}
+
+	private ProductRequest getProductRequest() {
+		return new ProductRequest("iPhone 13", "iPhone 13", "iPhone 13", "iPhone 13", BigDecimal.valueOf(1200));
+	}
+
+	@Test
+	void shouldListNoProducts() throws Exception {
+		RestAssured.given()
+				.contentType("application/json")
+				.when()
+				.get("/api/product")
+				.then()
+				.log().all()
+				.statusCode(200);
+	}
 }
